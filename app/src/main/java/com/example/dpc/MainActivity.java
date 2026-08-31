9aadb1510332e1600abcf6a78c60f457ee3a6340
sibwall <sibwall@atomicmail.io>
2026-08-31 08:55:53 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/dpc/MainActivity.java
+++ app/src/main/java/com/example/dpc/MainActivity.java
@@ -1,26 +1,25 @@
 package com.example.dpc;
 
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Динамически получаем имя пакета и ресивера
         String pkg = getPackageName();
         String admin = pkg + "/" + MyDeviceAdminReceiver.class.getName();
         int userId = 10;
 
-        // Формируем полный блок ADB-команд
         String adbCommands = 
             "adb shell pm create-user --profileOf 0 --user-type android.os.usertype.profile.MANAGED --user-id " + userId + " WorkProfile\n" +
             "adb shell am start-user " + userId + "\n" +
@@ -29,9 +28,8 @@ public class MainActivity extends AppCompatActivity {
             "adb shell dpm mark-profile-owner-on-organization-owned-device --user " + userId + " " + admin + "\n" +
             "adb shell am broadcast --user " + userId + " -a " + MyDeviceAdminReceiver.ACTION_APPLY_COPE + " -n " + admin;
 
-        // UI компоненты (программный layout)
-        android.widget.LinearLayout layout = new android.widget.LinearLayout(this);
-        layout.setOrientation(android.widget.LinearLayout.VERTICAL);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
         layout.setPadding(32, 32, 32, 32);
 
         TextView tvCommands = new TextView(this);
